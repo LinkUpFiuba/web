2c4f77e1fd95bb7f719e861380e6b060c36230d9
fedebrasburg <federico.brasburg@gmail.com>
2017-09-20 22:13:22 -0300

    Css modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div class={styles.general} className="App">
+      <div>
           fede
       </div>
     );
