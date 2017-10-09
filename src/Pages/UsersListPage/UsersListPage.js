6b907adc4fb2fe5c8c2b40e6cbd75887c80a9143
fedebrasburg <federico.brasburg@gmail.com>
2017-10-09 19:44:54 -0300

    agrego boton de reload

--- src/Pages/UsersListPage/UsersListPage.js
+++ src/Pages/UsersListPage/UsersListPage.js
@@ -4,6 +4,7 @@ import styles from './usersListPage.css'
 import ComplaintsList from '../../Components/ComplaintsList/ComplaintsList'
 import { loadComplaints } from '../../Services/ComplaintsService'
 import Loading from '../../Components/Loading/Loading'
+import { Button } from 'react-bootstrap'
 
 class UsersListPage extends Component {
 
@@ -18,12 +19,22 @@ class UsersListPage extends Component {
       .then( () => this.setState( { ready: true } ) )
   }
 
+  reloadComplaints = () => {
+    this.setState( { ready: false } )
+    loadComplaints()
+      .then( complaints => this.setState( { complaints } ) )
+      .then( () => this.setState( { ready: true } ) )
+  }
+
   render () {
     return (
       <div>
         <FullNavBar auth={this.props.auth} history={this.props.history}/>
         <div className={styles.body}>
-          <h1>Denuncias</h1>
+          <div className={styles.title}>
+            <h1>Denuncias</h1>
+            <Button className={styles.reloadComplaints} onClick={this.reloadComplaints}>  Recargar </Button>
+          </div>
           {!this.state.ready &&
             <Loading message="Cargando usuarios" size="150"/> }
           {this.state.ready &&
