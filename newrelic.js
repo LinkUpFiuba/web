832ba5a6ee6a2591c88cba7ca7116634e6032004
fedebrasburg <federico.brasburg@gmail.com>
2017-10-09 00:10:35 -0300

    new relic

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['link-up-web'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '2a912cbc2d3aa9d0174104e64e024a3b965d3df6',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
