4378b1b63d9640c80d06dc35aea3262e73fbe1b2
fedebrasburg <federico.brasburg@gmail.com>
2017-10-11 23:41:55 -0300

    Agrego cookies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import style from './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import style from './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <App className={style.body} />,
-  document.getElementById('root')
-);
-registerServiceWorker();
+  <App className={style.body}/>,
+  document.getElementById( 'root' )
+)
+registerServiceWorker()
