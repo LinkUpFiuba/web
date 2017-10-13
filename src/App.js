07b3e2e6ef459ca26150a4e002b67b7405be4fb6
fedebrasburg <federico.brasburg@gmail.com>
2017-10-12 22:12:52 -0300

    Agrego cookies (#6)

--- src/App.js
+++ src/App.js
@@ -1,55 +1,67 @@
-import React  from 'react';
-import LoginPage from './Pages/LoginPage/LoginPage';
+import React, { Component }  from 'react'
+import LoginPage from './Pages/LoginPage/LoginPage'
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-} from 'react-router-dom';
-import UsersListPage from './Pages/UsersListPage/UsersListPage';
+} from 'react-router-dom'
+import UsersListPage from './Pages/UsersListPage/UsersListPage'
 import ComplaintUserPage from './Pages/ComplaintUserPage/ComplaintUserPage'
+import Cookies from 'universal-cookie';
 
-const App = () => (
-  <Router>
-    <div>
-      <Route path="/login" render={() => <LoginPage auth={auth} />} />
-      <PrivateRoute path="/usersList/:userId" componentProps={{auth:auth}} component={ComplaintUserPage} />
-      <PrivateRoute path="/usersList" componentProps={{auth:auth}} component={UsersListPage} />
-      <PrivateRoute path="//" component={Home} />
-    </div>
-  </Router>
-);
+
+class App extends Component {
+
+
+  render(){
+    return (
+      <Router>
+        <div>
+          <button onClick={this.handleNameChange}> cookie</button>
+          <Route path="/login" render={() => <LoginPage auth={auth}/>}/>
+          <PrivateRoute path="/usersList/:userId" componentProps={{ auth: auth }} component={ComplaintUserPage}/>
+          <PrivateRoute path="/usersList" componentProps={{ auth: auth }} component={UsersListPage}/>
+          <PrivateRoute path="//" component={Home}/>
+        </div>
+      </Router>
+    )
+  }
+}
 
 const Home = () => {
-  return <Redirect to={'/usersList'} />;
-};
+  return <Redirect to={'/usersList'}/>
+}
+
+const cookies = new Cookies();
 
 const auth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true;
+  isAuthenticated: cookies.get( 'isSessionOpen' ) === 'true',
+  authenticate( cb ) {
+    this.isAuthenticated = true
+    cookies.set('isSessionOpen', true, { path: '/' });
   },
-  signout(cb) {
-    this.isAuthenticated = false;
-    setTimeout(cb, 10);
+  signout( cb ) {
+    this.isAuthenticated = false
+    cookies.set('isSessionOpen', false, { path: '/' });
+    setTimeout( cb, 10 )
   }
-};
+}
 
-
-const PrivateRoute = ({ componentProps: componentProps, component: Component, ...rest }) => (
+const PrivateRoute = ( { componentProps: componentProps, component: Component, ...rest } ) => (
   <Route exact
-    {...rest}
-    render={props =>
-      auth.isAuthenticated ? (
-        <Component {...componentProps} {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: '/login',
-            state: { from: props.location }
-          }}
-        />
-      )}
+         {...rest}
+         render={props =>
+           auth.isAuthenticated ? (
+             <Component {...componentProps} {...props} />
+           ) : (
+             <Redirect
+               to={{
+                 pathname: '/login',
+                 state: { from: props.location }
+               }}
+             />
+           )}
   />
-);
+)
 
-export default App;
+export default App
