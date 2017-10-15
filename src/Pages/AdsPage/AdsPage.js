9b74787aacc983055a6394731e7bfad62f7d4ef1
fedebrasburg <federico.brasburg@gmail.com>
2017-10-15 19:51:07 -0300

    Agrego funcionamiento para eliminar publicidades

--- src/Pages/AdsPage/AdsPage.js
+++ src/Pages/AdsPage/AdsPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import FullNavBar from '../../Components/FullNavBar'
 import styles from './AdsPage.css'
 import AdCard from './AdCard/AdCard'
-import { loadAds } from '../../Services/AdsService'
+import { deleteAd, loadAds } from '../../Services/AdsService'
 import Loading from '../../Components/Loading/Loading'
 
 class AdsPage extends Component {
@@ -18,10 +18,16 @@ class AdsPage extends Component {
       .then( () => this.setState( { ready: true } ) )
   }
 
+  handleDeleteAd = ( adUid ) => {
+    this.setState( { ready: false } )
+    deleteAd( adUid )
+      .then(() => this.componentDidMount())
+  }
+
   render () {
     const allCards = this.state.ready && this.state.ads.map( ad =>
         <div className={styles.grow}>
-          <AdCard ad={ad}/>
+          <AdCard deleteAd={this.handleDeleteAd} ad={ad}/>
         </div>
       )
 
--- src/Services/AdsService.js
+++ src/Services/AdsService.js
@@ -3,17 +3,24 @@ import { baseUrl } from '../Constants'
 export const loadAds = () => {
   return fetch( `${baseUrl}/ads` )
     .then( handleErrors )
-    .then( res => res.json())
+    .then( res => res.json() )
+}
+
+export const deleteAd = ( adUid ) => {
+  return fetch( `${baseUrl}/ads/${adUid}`, {
+    method: 'DELETE'
+  } )
+    .then( handleErrors )
 }
 
 function handleErrors ( response ) {
   if ( !response.ok ) {
-    console.log('Error:')
-    console.log( response)
+    console.log( 'Error:' )
+    console.log( response )
     if ( response.status === 404 ) {
       throw { status: response.status, message: response.message }
     }
-    console.log(response.message)
+    console.log( response.message )
     throw Error( response.status )
   }
   return response
