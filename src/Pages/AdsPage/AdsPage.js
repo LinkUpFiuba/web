05ce3e6ee5b1667b3a2d8c924f11d36bc23bc707
fedebrasburg <federico.brasburg@gmail.com>
2017-10-16 16:43:02 -0300

    Crear publicidad (#8)

--- src/Pages/AdsPage/AdsPage.js
+++ src/Pages/AdsPage/AdsPage.js
@@ -2,20 +2,39 @@ import React, { Component } from 'react'
 import FullNavBar from '../../Components/FullNavBar'
 import styles from './AdsPage.css'
 import AdCard from './AdCard/AdCard'
-import { loadAds } from '../../Services/AdsService'
+import { loadAds, createAd } from '../../Services/AdsService'
 import Loading from '../../Components/Loading/Loading'
+import Button from 'react-bootstrap/es/Button'
+import CreateAdModal from './CreateAdModal/CreateAdModal'
 
 class AdsPage extends Component {
 
   state = {
     ads: [],
     ready: false,
+    showModal: false
   }
 
   componentDidMount = () => {
     loadAds()
       .then( ads => this.setState( { ads } ) )
       .then( () => this.setState( { ready: true } ) )
+  }
+
+  onClickCreateAd = ( title, image, state ) => {
+    this.setState( { showModal: false, ready: false } )
+    createAd( { title: title, image: image, state: state } )
+      .then( () => {
+        this.componentDidMount()
+      } )
+  }
+
+  handleModalClose = () => {
+    this.setState( { showModal: false } )
+  }
+
+  handleCreateAdButtonOnClick = () => {
+    this.setState( { showModal: true } )
   }
 
   render () {
@@ -29,13 +48,17 @@ class AdsPage extends Component {
       <div>
         <FullNavBar auth={this.props.auth} history={this.props.history}/>
         <div className={styles.body}>
-          <h1>Publicidades</h1>
+          <div className={styles.title}>
+            <h1>Publicidades</h1>
+            <Button className={styles.createButton} onClick={this.handleCreateAdButtonOnClick}> Crear publicidad </Button>
+          </div>
           {!this.state.ready &&
           <Loading message="Cargando publicidades" size={150}/> }
           {this.state.ready &&
           <div className={styles.adsList}>
             {allCards}
           </div>}
+          <CreateAdModal create={this.onClickCreateAd} show={this.state.showModal} onClose={this.handleModalClose}/>
         </div>
       </div>
     )
--- src/Services/AdsService.js
+++ src/Services/AdsService.js
@@ -3,17 +3,36 @@ import { baseUrl } from '../Constants'
 export const loadAds = () => {
   return fetch( `${baseUrl}/ads` )
     .then( handleErrors )
-    .then( res => res.json())
+    .then( res => res.json() )
+}
+
+export const createAd = ( ad ) => {
+  return fetch( `${baseUrl}/ads`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        title: ad.title,
+        image: ad.image,
+        state: ad.state
+      })
+    },
+  ).then( handleErrors )
+    .catch( err => {
+      console.log( err )
+    } )
 }
 
 function handleErrors ( response ) {
   if ( !response.ok ) {
-    console.log('Error:')
-    console.log( response)
+    console.log( response )
     if ( response.status === 404 ) {
       throw { status: response.status, message: response.message }
     }
-    console.log(response.message)
+    console.log( response.message )
     throw Error( response.status )
   }
   return response
