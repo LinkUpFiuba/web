38789f282953d3b7f08418099c9c459f31376642
Pablo Ciruzzi <p.ciruzzi@hotmail.com>
2017-11-13 01:01:24 -0300

    Use production backend

--- src/Constants.js
+++ src/Constants.js
@@ -2,5 +2,5 @@ export const adminUser = 'admin';
 export const adminPass = 'admin';
 
 // export const baseUrl = 'http://localhost:5000'
-export const baseUrl = 'https://dev-link-up-g1.herokuapp.com'
-// export const baseUrl = 'https://link-up-g1.herokuapp.com'
+// export const baseUrl = 'https://dev-link-up-g1.herokuapp.com'
+export const baseUrl = 'https://link-up-g1.herokuapp.com'
